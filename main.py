a638490036792c2d1da843ba9ae3042d23cfd661
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-07-04 11:48:38 +0700

    implement error handler when a video is not found

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,8 +11,13 @@ video_put_args.add_argument("likes", type=int, help="Likes on the video", requir
 
 videos = {}
 
+def abort_if_video_id_doesnt_exist(video_id):
+  if video_id not in videos:
+    abort(404, message="Video id is not valid...")
+
 class Video(Resource):
   def get(self, video_id):
+    abort_if_video_id_doesnt_exist(video_id)
     return videos[video_id]
   
   def put(self, video_id):
